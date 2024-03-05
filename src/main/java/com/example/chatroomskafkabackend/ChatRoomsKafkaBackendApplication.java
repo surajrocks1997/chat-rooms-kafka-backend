8bd57791564d0833eda7915a0f662ec21b10317a
Suraj Gupta <surajrocks1997@gmail.com>
2024-03-05 22:38:48 +0530

    init commit

---
+++ src/main/java/com/example/chatroomskafkabackend/ChatRoomsKafkaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatroomskafkabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomsKafkaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomsKafkaBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/chatroomskafkabackend/config/WebSocketConfig.java
@@ -0,0 +1,4 @@
+package com.example.chatroomskafkabackend.config;
+
+public class WebSocketConfig {
+}
